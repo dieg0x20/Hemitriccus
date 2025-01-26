b5bfde7778bc486b379a51c40e4cd973129e0517
Diego <diego444x@gmail.com>
2025-01-26 01:25:01 -0300

    incluindo pacote click

--- src/steganography.py
+++ src/steganography.py
@@ -1,26 +1,29 @@
-# Author -> dieg0x20
-# Script de esteganografia em python usando a tecnica de LSB
-# Instale os requerimentos necessarios para rodar o arquivo com o camando pip install requirements.txt
 from PIL import Image
 import os
+import click
 
-# Converte a mensagem em binário
+
+@click.group()
+def cli():
+    pass
+
 def message_to_bin(message):
     print("Mensagem binária: ", ''.join(format(ord(char), '08b') for char in message))
     return ''.join(format(ord(char), '08b') for char in message)
 
-# Converte o binário de volta para a mensagem
-def bin_to_message(binary_message):
-    chars = [binary_message[i:i+8] for i in range(0, len(binary_message), 8)]
-    return ''.join(chr(int(char, 2)) for char in chars)
+@cli.command()
+@click.option('--message', prompt='Mensagem que deseja encriptar', help='Escreva a mensagem que deseja encriptar')
+@click.option('--image', '-i', prompt='Imagem que será modificada', help='Caminho da imagem para ocultar a mensagem')
+def encode_message(message, image):
+    output_image_path = os.path.join(os.getcwd(), 'encoded_image.png')
 
-# Esconde a mensagem dentro da imagem
-def encode_message(image_path, message, output_image_path):
-    img = Image.open(image_path)
+    # Abre a imagem
+    img = Image.open(image)
     img = img.convert('RGB')
-    pixels = img.load()  # Corrigido o nome da variável
+    pixels = img.load()
 
-    bin_message = message_to_bin(message) + '11111111'  # Adiciona um delimitador
+    # Converte a mensagem em binário e adiciona o delimitador
+    bin_message = ''.join(format(ord(char), '08b') for char in message) + '11111111'
     print("Mensagem binária: ", bin_message)
     message_len = len(bin_message)
     width, height = img.size
@@ -33,14 +36,21 @@ def encode_message(image_path, message, output_image_path):
                 r_bin = format(r, '08b')
                 r_bin = r_bin[:7] + bin_message[idx]  # Substitui o último bit
                 r = int(r_bin, 2)
-                pixels[x, y] = (r, g, b)  # Atualiza o pixel com o valor modificado
+                pixels[x, y] = (r, g, b)
                 idx += 1
             else:
-                img.save(output_image_path)  # Salva a imagem modificada
-                return f"Mensagem escondida e salva no {output_image_path}"
+                img.save(output_image_path)
+                click.echo(f"Mensagem escondida e salva no {output_image_path}")
+                return
 
-def decode_message(image_path):
-        img = Image.open(image_path)
+    click.echo(f"Mensagem escondida e salva no {output_image_path}")
+
+
+
+@cli.command()
+@click.option('--image', '-i', prompt='Imagem que será modificada', help='Caminho da imagem para ocultar a mensagem')
+def decode_message(image):
+        img = Image.open(image)
         img = img.convert('RGB')
         pixels = img.load()
 
@@ -52,30 +62,39 @@ def decode_message(image_path):
                 r, g, b = pixels[x, y]
                 bin_message += format(r, '08b')[-1]  #pega o ultimo bit significativo
 
-                if bin_message[-8:]== '11111111' and len(bin_message)%8==0 :
-                    return bin_to_message(bin_message[:-8])
-
+                if bin_message[-8:]== '11111111' and len(bin_message)%8==0 :  
+                   decoded_message = bin_to_message(bin_message[:-8])
+                   click.echo(f"Mensagem decodificada: {decoded_message}")
+                   return decode_message
+    
         return "Nenhuma menssage encontrada"
 
-def main():
-    image_path = input("Digite o caminho da imagem: ")
+def bin_to_message(binary_message):
+    # Converte a mensagem binária para texto
+    chars = [binary_message[i:i+8] for i in range(0, len(binary_message), 8)]
+    return ''.join(chr(int(char, 2)) for char in chars)
 
-    if not os.path.exists(image_path):
-        print("Imagem não encontrada.")
-        return
+cli.add_command(encode_message)
+cli.add_command(decode_message)
 
-    secret_message = input("Digite a mensagem secreta que deseja esconder: ")
+#def main():
+    #image_path = input("Digite o caminho da imagem: ")
+
+    #if not os.path.exists(image_path):
+    #    print("Imagem não encontrada.")
+    #    return
+
+    #secret_message = input("Digite a mensagem secreta que deseja esconder: ")
 
     # Define o caminho de saída da imagem modificada
-    output_image_path = os.path.join(os.getcwd(), 'encoded_image.png')
+    #output_image_path = os.path.join(os.getcwd(), 'encoded_image.png')
 
     # Codifica a mensagem na imagem
-    encode_result = encode_message(image_path, secret_message, output_image_path)
-    print(encode_result)
+    #encode_result = encode_message(image_path, secret_message, output_image_path)
+    #print(encode_result)
 
     #decodificar a imagem
-    decoded_message = decode_message(output_image_path)
-    print("A mensagem decodificada: ", decoded_message)
-
+    #decoded_message = decode_message(output_image_path)
+    #print("A mensagem decodificada: ", decoded_message)
 if __name__ == "__main__":
-    main()
+    cli()
